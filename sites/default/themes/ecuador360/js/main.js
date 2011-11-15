a56cdec586208ec4dcf7180a382d0ed808d06c52
Cesar Montalvo <cmontalvo@devsu.com>
2011-11-14 19:54:18 -0500

    fix map

--- sites/default/themes/ecuador360/js/main.js
+++ sites/default/themes/ecuador360/js/main.js
@@ -15,6 +15,8 @@
       $('.i18n-es .menu-restaurants').attr('href', '/es/content/category-result?category=104');
       $('.i18n-en .menu-ranking').attr('href', '/content/category-result?category=all');
       $('.i18n-es .menu-ranking').attr('href', '/es/content/category-result?category=all');
+      //Add controls to map
+
     }
   };
 }(jQuery));
@@ -22,25 +24,64 @@
 //Map Filter
 (function ($) {
   $(document).ready(function() {
-    $('#map-filter').click(function(){
-      //Hide all markers
-      $('#gmap-map360-gmap0 img').each(function() {
-        if($(this).attr('id').indexOf('mtgt_unnamed_', 0) == 0) {
-          $(this).addClass('hidden');
-        }
-      });
-      $('.map-footer-wrapper .category input:checked').each(function() {
-        //alert ($(this).closest('.category').children('.title').html());
-        var type = $(this).closest('.category').children('.title').attr('rel');
-        //Show filtered markers
-        $('#gmap-map360-gmap0 img').each(function() {
-          if($(this).attr('id').indexOf('mtgt_unnamed_', 0) == 0) {
-            if($(this).attr('src').indexOf(type, 0) >= 0) {
-              $(this).removeClass('hidden');
-            }
+    //All selected
+    $('.map-footer input').click(function() {
+      if($(this).attr('id') == '-1') {
+        $('.map-footer-wrapper .category input:checked').each(function() { 
+          if($(this).attr('id') != '-1') {
+            $(this).attr('checked', false);
           }
         });
+      } else {
+        if($(this).attr('checked') == true) {
+          $('.map-footer input#-1').attr('checked', false);
+        }
+      }
+    });
+    //Filter Click
+    $('#map-filter').click(function() {
+//      var map = new GMap2(document.getElementById("gmap-map360-gmap0"));
+//      map.setCenter(new GLatLng(37.4419, -122.1419), 13);
+//      map.addControl(new GSmallMapControl());
+//      map.addControl(new GMapTypeControl());
+      showAll = true;
+      $('.map-footer-wrapper .category input:checked').each(function() {
+        showAll = false;
       });
+      if(showAll) {
+        if($('.i18n-es').offset().top > 0) {
+          alert('Debe seleccionar un filtro por lo menos.');
+        } else {
+          alert('You must select at least one filter.');
+        }
+      } else {
+        //Hide all markers
+        $('#gmap-map360-gmap0 img').each(function() {
+          if($(this).attr('id').indexOf('mtgt_unnamed_', 0) == 0) {
+            $(this).addClass('hidden');
+          }
+        });
+        $('.map-footer-wrapper .category input:checked').each(function() {
+          var type = $(this).closest('.category').children('.title').attr('rel');
+          showAll = false;
+          if($(this).attr('id') == -1) {
+            showAll = true;
+          }
+          //Show filtered markers
+          $('#gmap-map360-gmap0 img').each(function() {
+            if($(this).attr('id').indexOf('mtgt_unnamed_', 0) == 0) {
+              if(showAll) {
+                $(this).removeClass('hidden');
+              }
+              else {
+                if($(this).attr('src').indexOf(type, 0) >= 0) {
+                  $(this).removeClass('hidden');
+                }
+              }
+            }
+          });
+        });
+      }
     });
   });
 }(jQuery));
@@ -48,8 +89,7 @@
 function mypopup(url) {
   mywindow = window.open
                (url +"","Ecuador360","width=800,height=400,top=200,left=230,resizable=1");
-  //mywindow.moveTo(100,200);
- }
+}
 
  /**
  * Function to render social share overlay
