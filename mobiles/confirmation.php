b93b6196504d93f2ab883662a1024f6c09ba896e
Cesar Montalvo <cmontalvo@oshyn.com>
2011-12-09 18:00:01 -0500

    Mobile App Files

---
+++ mobiles/confirmation.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Confirmation</title>
+<style type="text/css">
+body
+{
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+.logo_center
+{
+   position:absolute;
+   top:50%;
+   left:50%;
+   margin-top:-28px;
+   margin-left:-130px;
+}
+</style>
+</head>
+<body style="background-color:#000">
+<img class="logo_center" src="ecuador360_image.png" width="260" height="56" />
+</body>
+</html>
---
+++ mobiles/terms.php
@@ -0,0 +1,20 @@
+<?php
+$loc = $_GET['loc'];
+if(!isset($loc) || $loc != 'es')
+	$loc = 'en';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?php echo ($loc == 'es' ? 'Términos de Servicio' : 'Terms of Service'); ?></title>
+</head>
+<body style="background-color:#000">
+<div style="text-align:center;color:#FFF;font-family:Verdana, Geneva, sans-serif;font-size:medium">
+      <img src="ecuador360_image.png" width="260" height="56" />
+      <h2>Terms of Service</h2>
+      <p>These Terms of Service (&quot;<strong>Terms</strong>&quot;) govern your access to and use of the services and Ecuador360’s websites (the &quot;<strong>Services</strong>&quot;),   and any information, text, graphics, photos or other materials   uploaded, downloaded or appearing on the Services (collectively referred   to as &quot;<strong>Content</strong>&quot;). Your access to and use of the   Services is conditioned on your acceptance of and compliance with these   Terms. By accessing or using the Services you agree to be bound by these   Terms. </p>
+      <p>&nbsp;</p>
+</div>
+</body>
+</html>
